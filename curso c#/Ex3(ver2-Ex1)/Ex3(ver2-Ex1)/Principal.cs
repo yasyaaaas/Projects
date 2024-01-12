30c8b9f37e43238323744113fe894c64365bbd90
Yasmin Cassemiro Viegas <130803911+yasyaaaas@users.noreply.github.com>
2024-01-12 14:47:12 -0300

    Ex3 upload

---
+++ curso c#/Ex3(ver2-Ex1)/Ex3(ver2-Ex1)/Principal.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Ex3_ver2_Ex1_ {
+    internal class Principal {
+        static void Main(string[] args) {
+           Produto p = new Produto("TV", 500.00, 10);
+            p.Nome = "T";
+
+            Console.WriteLine(p.Nome);
+            Console.WriteLine(p.Preco);
+        }
+    }
+}
---
+++ curso c#/Ex3(ver2-Ex1)/Ex3(ver2-Ex1)/Produto.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex3_ver2_Ex1_ {
+    internal class Produto {
+        private string _nome;
+        public double Preco { get; private set; }
+        public int Quantidade { get; private set; }
+
+        public Produto() {
+        }
+
+        public Produto(string nome, double preco, int quantidade) {
+            _nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+
+        public Produto(string nome, double preco) {
+            _nome = nome;
+            Preco = preco;
+            Quantidade = 0;
+        }
+
+        public string Nome {
+            get { return _nome; }
+            set {
+                if(value != null && value.Length > 1) {
+                    _nome = value;
+                }
+            }
+        }
+
+        public double ValorTotalEmEstoque() {
+            return Preco * Quantidade;
+        }
+
+        public void AdicionarProdutos(int quantidade) {
+            this.Quantidade += quantidade;
+        }
+
+        public void RemoverProdutos(int quantidade) {
+            this.Quantidade -= quantidade;
+        }
+
+        public override string ToString() {
+            return _nome + ", $ " + Preco.ToString("F2") + ", " + Quantidade + " unidades, Total: $ " + ValorTotalEmEstoque().ToString("F2");
+        }
+    }
+}
