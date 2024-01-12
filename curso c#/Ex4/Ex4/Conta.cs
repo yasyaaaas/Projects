9e75d3de2be29f40842268393539330ca35e5ac4
Yasmin Cassemiro Viegas <130803911+yasyaaaas@users.noreply.github.com>
2024-01-12 15:10:43 -0300

    Ex4 upload

---
+++ curso c#/Ex4/Ex4/Conta.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex4 {
+    internal class Conta {
+        public int Numero { get; private set; }
+        public string Titular { get; set; }
+        public double Saldo { get; private set; }
+
+        public Conta(int numero, string titular) {
+            Numero = numero;
+            Titular = titular;
+            Saldo = 0.0;
+        }
+
+        public Conta(int numero, string titular, double saldo) : this(numero, titular) {
+           Deposito(saldo);
+        }
+
+        public void Deposito(double valor) {
+            Saldo += valor;
+        }
+
+        public void Saque(double valor) {
+            Saldo -= valor + 5.0;
+        }
+
+        override public string ToString() {
+            return $"Conta {Numero}, Titular: {Titular}, Saldo: ${Saldo}";
+        }
+    }
+}
---
+++ curso c#/Ex4/Ex4/Principal.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Ex4 {
+    internal class Principal {
+        static void Main(string[] args) {
+            Conta conta;
+            Console.Write("Entre o número da conta: ");
+            int numero = int.Parse(Console.ReadLine());
+            Console.Write("Entre o titular da conta: ");
+            string titular = Console.ReadLine();
+            Console.Write("Haverá depósito inicial (s/n)? ");
+            char resposta = char.Parse(Console.ReadLine());
+            if (resposta == 's' || resposta == 'S') {
+                Console.Write("Entre o valor de depósito inicial: ");
+                double depositoInicial = double.Parse(Console.ReadLine());
+                conta = new Conta(numero, titular, depositoInicial);
+            } else {
+                conta = new Conta(numero, titular);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta: ");
+            Console.WriteLine(conta);
+
+            Console.WriteLine();
+            Console.Write("Entre um valor para depósito: ");
+            double valor = double.Parse(Console.ReadLine());
+            conta.Deposito(valor);
+            Console.WriteLine("Dados da conta atualizados: ");
+            Console.WriteLine(conta);
+
+            Console.WriteLine();
+            Console.Write("Entre um valor para saque: ");
+            valor  = double.Parse(Console.ReadLine());
+            conta.Saque(valor);
+            Console.WriteLine("Dados da conta atualizados: ");
+            Console.WriteLine(conta);
+        }
+    }
+}
