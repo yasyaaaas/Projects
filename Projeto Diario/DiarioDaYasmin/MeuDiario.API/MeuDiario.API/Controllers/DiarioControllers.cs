e089d2db9f2c41e13b01902df334c0fb09c35299
Yasmin Cassemiro Viegas <130803911+yasyaaaas@users.noreply.github.com>
2024-03-10 11:32:31 -0300

    Projeto Diario

---
+++ Projeto Diario/DiarioDaYasmin/MeuDiario.API/MeuDiario.API/Controllers/DiarioControllers.cs
@@ -0,0 +1,40 @@
+﻿using MeuDiario.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MeuDiario.API.Controllers {
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DiarioControllers : ControllerBase {
+        private static List<Diario> _diario = new List<Diario>();
+        private static int nextId = 1;
+
+        [HttpPost] // colocar na lista
+        public IActionResult Post(Diario diario) {
+            diario.Id = nextId++;
+            _diario.Add(diario);
+            return Ok(diario);
+        }
+
+        [HttpGet] // mostrar lista
+        public IActionResult GetAll() {
+            return Ok(_diario);
+        }
+
+        [HttpGet("{id}")] // pega a lista por id
+        public IActionResult Get(int id) {
+            var diario = _diario.FirstOrDefault(diario => diario.Id == id);
+            return Ok(diario);
+        }
+
+        [HttpDelete] // deleta algum da lista
+        public IActionResult Delete(int id) {
+            var diario = _diario.FirstOrDefault(diario => diario.Id == id);
+            if (diario == null) {
+                return NotFound();
+            }
+            _diario.Remove(diario);
+            return Ok(_diario);
+        }
+    }
+}
---
+++ Projeto Diario/DiarioDaYasmin/MeuDiario.API/MeuDiario.API/Models/Diario.cs
@@ -0,0 +1,10 @@
+﻿namespace MeuDiario.API.Models {
+    public class Diario {
+        public int Id { get; set; }
+        public string Name { get; set;}
+        public string Date { get; set; }
+        public string Type { get; set; }
+        public int Nota { get; set;}
+
+    }
+}
